955ee8a24a936af7af5968b2ed1773e201da4958
curitiba <curitiba@MJV-DLWZFM2.MJV.net>
2020-07-05 17:49:18 -0300

    Animated hamburger menu

---
+++ app/js/script.js
@@ -0,0 +1,19 @@
+const btnHamburguer = document.querySelector('#btn-hamburguer');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+header.addEventListener('click', function() {
+    console.log('open hamburguer');
+    if (header.classList.contains('open')) {
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else {
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+
+
+
+})
